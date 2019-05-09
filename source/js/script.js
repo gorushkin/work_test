80bb3c27f3c71a520f7c2e850131a08476d94b54
Artyom Gorushkin <gorushkin@gmail.com>
2019-05-10 00:49:46 +0300

    закончил

--- source/js/script.js
+++ source/js/script.js
@@ -3,6 +3,8 @@ let linkBtn = document.querySelector(".navbar__link--callback");
 let popUp = document.querySelector('.popup');
 let overlay = document.querySelector('.overlay');
 let popUpCloseBtn = popUp.querySelector('.popup__close');
+let menuOpenButton = document.querySelector('.header__menubutton');
+let navBar = document.querySelector('.navbar');
 
 linkBtn.addEventListener('click', function (evt) {
   evt.preventDefault();
@@ -16,4 +18,20 @@ linkBtn.addEventListener('click', function (evt) {
   })
 })
 
+menuOpenButton.addEventListener('click', function(evt) {
+  evt.preventDefault();
+  navBar.classList.toggle("navbar--show");
+})
 
+
+var swiper = new Swiper('.swiper-container', {
+  pagination: {
+    el: '.swiper-pagination',
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  loop: true
+});
